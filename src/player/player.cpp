078a9c4eb8c1f2f3434471a559ea452e2d4a4c47
maniekx86 <maniekx086@gmail.com>
2023-08-16 01:33:21 +0200

    player.cpp quick cleanup

--- src/player/player.cpp
+++ src/player/player.cpp
@@ -12,6 +12,7 @@
 #include "../headers/sprite.hpp"
 #include "../headers/luaapi.hpp"
 #include "../headers/commonapi.hpp"
+#include "../headers/cli.hpp"
 
 PityBoy::PBWindow win;
 
@@ -21,18 +22,6 @@ int main(int argc, char* argv[]) {
     PityBoy::PBSprite testSprite;
     PityBoy::CommonAPI::mainWindow = &win;
     PityBoy::LuaEngine engine; 
-
-    if (argv[0] == "-h") { // it was index 0 right compile again    
-        std::cout << "placeholder" << std::endl;
-    } else {
-        std::filesystem::path cartPath = std::filesystem::path(argv[1]);
-
-        if (std::filesystem::exists(cartPath) && std::filesystem::is_regular_file(cartPath) && cartPath.extension() == "\".pit\"") {
-            std::cout << "yeah its fineeee" << std::endl;
-        } else {
-            std::cout << "invalid path for cartridge" << std::endl;
-        }
-    }
 
     engine.initApi();
     engine.execute("function render() text(10,10,'im so fucking cool', 1) end");
